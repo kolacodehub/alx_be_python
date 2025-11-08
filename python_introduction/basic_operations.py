cc2c4c6571da9a9a3b324458fb1161f2221921aa
Tiamiyu Abdul-salam <128032488+kolacodehub@users.noreply.github.com>
2025-11-08 08:41:17 +0100

    added basic operation

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+difference = number2 + number1
+multiplication = number1 * number2
+
+print(f'Addition of {number1} and {number2} is {sum}')
+print(f'Subtraction of {number1} and {number2} is {difference}')
+print(f'multiplication of {number1} and {number2} is {multiplication}')
