3507b9bb15304ef8678b8bbfb161a61b2a17055d
Tiamiyu Abdul-salam <128032488+kolacodehub@users.noreply.github.com>
2025-11-08 08:43:14 +0100

    edited subtraction

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,7 +1,7 @@
 number1 = 10
 number2 = 5
 sum = number1 + number2
-difference = number2 + number1
+difference = number2 - number1
 multiplication = number1 * number2
 
 print(f'Addition of {number1} and {number2} is {sum}')
