7b6ef5ea7767e021d58ba495683c33e77e5138e2
Tiamiyu Abdul-salam <128032488+kolacodehub@users.noreply.github.com>
2025-12-13 10:58:19 +0100

    yh

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,7 +6,6 @@ def perform_operation(num1, num2, operation):
     elif (operation == 'multiply'):
         return num1 * num2
     elif (operation == 'divide'):
-        try:
-            return num1 / num2
-        except ZeroDivisionError:
+        if num2 == 0:
             return "Error: Division by zero is not allowed."
+        return num1 / num2
