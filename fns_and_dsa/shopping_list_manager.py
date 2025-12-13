782ed1831afc98424bee0146b9e3acb5d6a67770
Tiamiyu Abdul-salam <128032488+kolacodehub@users.noreply.github.com>
2025-12-13 15:15:44 +0100

    functionality added

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,44 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            item = input("Enter item to add: ")
+            shopping_list.append(item)
+            print(f"{item} added succesfully")
+            pass
+        elif choice == "2":
+            item = input("Enter item to remove: ")
+            if (item in shopping_list):
+                shopping_list.remove(item)
+                print(f"{item} removed succesfully")
+            else:
+                print(f'{item} not found in Shopping List')
+            pass
+        elif choice == "3":
+            if (len(shopping_list) != 0):
+                for item in shopping_list:
+                    print('Items in the Shopping List are: ')
+                    print(item)
+            else:
+                print('No item in shopping list')
+            pass
+        elif choice == "4":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
